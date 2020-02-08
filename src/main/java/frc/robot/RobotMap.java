7b14dc359d3dd6d048b73c5ab6f760206038fa41
LudingtonRobotics <34400009+LudingtonRobotics@users.noreply.github.com>
2020-02-08 11:09:00 -0500

    Create RobotMap.java

---
+++ src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,18 @@
+package frc.robot;
+
+class RobotMap{
+
+    /************DriveTrain IDS*************/
+    public static final int _rghtMain  =  10;
+    public static final int _rghtFol1  =  11;
+    public static final int _rghtFol2  =  12;
+    public static final int _leftMain  =  20;
+    public static final int _leftFol1  =  21;
+    public static final int _leftFol2  =  22;
+    /************DriveTrain IDS*************/
+
+     public static final int _colrWhel  =  30;
+
+
+
+}
