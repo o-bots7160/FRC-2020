5f5e8797f3f08e19bae6139b742336c39b7c7079
LudingtonRobotics <34400009+LudingtonRobotics@users.noreply.github.com>
2020-02-14 16:44:26 -0500

    Updated enum class

--- src/main/java/frc/robot/ColorMap.java
+++ src/main/java/frc/robot/ColorMap.java
@@ -1,7 +1,17 @@
 package frc.robot;
 
 enum ColorMap{
-     Rainbow,Red,SkyBlue,White,Green,Yellow,ColorGradient;
+     Rainbow(-0.97),Red(0.61),SkyBlue(0.83),White(0.93),Green(0.69),Yellow(0.69),ColorGradient(0.41);
+
+     private ColorMap(final double colorCodes){
+        this.colorCodes = colorCodes;
+     }
+
+     private double colorCodes;
+
+     public double getColor(){
+         return colorCodes;
+     }
 
 
 
--- src/main/java/frc/robot/Led.java
+++ src/main/java/frc/robot/Led.java
@@ -34,32 +34,7 @@ public class Led {
   public void testPeriodic() {
   }
   public void  setColor( ColorMap newColor ) {
-    switch ( newColor ) {
-      case Rainbow:
-        LEDs.set( -0.97 );
-        break;
-      case Red :
-        LEDs.set( 0.61 );
-        break;
-      case SkyBlue:
-        LEDs.set( 0.83 );
-        break;
-      case White:
-        LEDs.set( 0.93 );
-        break; 
-      case Green:
-        LEDs.set( 0.77 );
-        break; 
-      case Yellow:
-        LEDs.set( 0.69 );
-        break;
-      case ColorGradient:
-        LEDs.set( 0.41 );
-        break;
-      default:
-        LEDs.set( 0.41 );
-      break;
-  }
+    LEDs.set(newColor.getColor());
 }
 
 }
